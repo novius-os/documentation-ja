cb5ae1ace0d80a48e270e7dfd256b6efe3e9e458
Gilles FELIX <felix@novius.com>
2013-07-15 09:50:02 +0200

    Update conf

--- source/conf.py
+++ source/conf.py
@@ -41,7 +41,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Novius OS'
-copyright = u'2012, Novius'
+copyright = u'2012, Novius OS'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
